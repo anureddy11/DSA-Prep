db43a542e5d665cb3e74883a2830afb9fb33f670
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-06-25 20:10:17 -0700

    two jewels

---
+++ twojewels.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        jewels_set = jewels
+
+        for stone in stones:
+            if stone in jewels_set:
+                count+=1
+
+        return count
